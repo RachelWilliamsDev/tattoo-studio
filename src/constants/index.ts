4d803545af11d2b6e0b4625587c284d365e3ef1d
RachelWilliamsDev <Rachel@stacktiger.co>
2024-11-13 10:16:37 +0000

    chore: added assets and constants

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const navigation = [
+  {
+    name: "Home Page",
+    href: "/",
+  },
+  {
+    name: "Portfolio",
+    href: "#",
+  },
+  {
+    name: "About Us",
+    href: "#",
+  },
+  {
+    name: "Inquiries",
+    href: "#",
+  },
+  {
+    name: "Pricings",
+    href: "#",
+  },
+  {
+    name: "Challenges",
+    href: "#",
+  },
+];
