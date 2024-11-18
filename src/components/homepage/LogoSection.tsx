6db218bb56d971ddddc2d7a06c9d29b6ae16275e
RachelWilliamsDev <Rachel@stacktiger.co>
2024-11-18 12:44:59 +0000

    Fix file name casing for logo section

--- src/components/homepage/LogoSection.tsx
+++ src/components/homepage/LogoSection.tsx
@@ -1,7 +1,7 @@
+import Container from "@/components/global/Container";
 import { logos } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import Container from "../global/Container";
 
 const logoSection = () => {
   return (
